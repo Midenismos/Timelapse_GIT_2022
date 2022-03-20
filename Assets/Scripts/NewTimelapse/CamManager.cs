31805640d64f138bb5fa4b583fc735b311382da6
snopingas <n.cadre@edu.institutartline.com>
2022-03-20 12:43:26 +0100

    Ajout de la timeline et changement de taille et de placement des cams et des boutons

--- Assets/Scripts/NewTimelapse/CamManager.cs
+++ Assets/Scripts/NewTimelapse/CamManager.cs
@@ -2,13 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Video;
+using UnityEngine.UI;
 
 
 public class CamManager : MonoBehaviour
 {
     [SerializeField] private VideoPlayer[] _cams;
     [SerializeField] private bool _isRewinding = false;
+    [SerializeField] private Slider _slider;
+    public bool IsSliderClicked = false;
 
+    private void Awake()
+    {
+        _slider.maxValue = (float)_cams[0].clip.length;
+    }
     private void Update()
     {
         //Rembobine les vidéos
@@ -17,6 +24,14 @@ public class CamManager : MonoBehaviour
             foreach(VideoPlayer cam in _cams)
                 cam.time = _cams[0].time - 1;
         }
+        if(!IsSliderClicked)
+        {
+            _slider.value = (float)_cams[0].time;
+        }
+
+
+        if ((float)_cams[0].time <= _slider.value+0.1f && (float)_cams[0].time >= _slider.value - 0.1f)
+            IsSliderClicked = false;
     }
     public void Rewind()
     {
@@ -47,6 +62,32 @@ public class CamManager : MonoBehaviour
         _isRewinding = false;
         foreach (VideoPlayer cam in _cams)
             cam.playbackSpeed = 3;
+    }
+
+    public void ChangeTime()
+    {
+        foreach (VideoPlayer cam in _cams)
+        {
+            cam.time = _slider.value;
+            cam.Play();
+        }
+
+    }
+
+    public void StopSlider()
+    {
+        if (GameObject.Find("Player").GetComponent<PlayerAxisScript>().IDCurrentAxis == 1)
+        {
+            foreach (VideoPlayer cam in _cams)
+                cam.Pause();
+            IsSliderClicked = true;
+        }
+
+
+    }
+    public void ReactivateSlider()
+    {
+        ChangeTime();
     }
 }
 
